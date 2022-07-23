55e5f5d1dc41c2fad6ca4554ee645036108d1d12
ct0r <ct0r@protonmail.com>
2022-07-23 20:49:31 +0200

    Deploy

---
+++ _app/immutable/pages/about.svelte-4ae05097.js
@@ -0,0 +1,29 @@
+import{S as D,i as B,s as H,x as M,k as y,e as d,t as b,y as R,m as w,c as f,a as m,h as k,d as a,b as p,z as W,g as P,F as o,n as Y,r as q,p as F,C as O}from"../chunks/index-266cbc9e.js";import{T as V}from"../chunks/Title-b1fe37bf.js";function j(U){let n,g,r,e,s,_,x,l,I,T,c,A,E,h,$,v;return n=new V({props:{text:"About"}}),{c(){M(n.$$.fragment),g=y(),r=d("section"),e=d("div"),s=d("p"),_=b(`I am a nym enthusiast who belives one's privacy is too high a price to pay for using the
+			internet. Nym network could bring an alternative option. However I belive that for the network
+			to be trustworthy it needs to be diversified - that is, diversified in many aspects.`),x=y(),l=d("p"),I=b(`Take for instance infrastructure. As of now, large portion of mixnet nodes is running on the
+			infrastructure of only a small number of cloud providers. What is the purpose of mixnet
+			splitting traffic among numerous nodes if these nodes are running on the infrastructure of a
+			single provider?`),T=y(),c=d("p"),A=b(`Unfortunately a monitoring tool is missing that would evaluate the nodes not only from the
+			economical point of view (the more you stake, the more you earn) but would also take into
+			account aspects such as geolocation, infrustructure, etc. In short, it is difficult to decide
+			which nodes to stake to if you want to contribute to the health of a diversified network
+			rather than simply seek the highest profit. Being a rust/blockchain developer, I have some
+			ideas regarding such a tool... STAY TUNED!`),E=y(),h=d("p"),$=b(`My first real contribution to the general idea hinted above is my node. It is running at a
+			local datacenter in the Czech Republic, which is owned by a rather large Italian cloud
+			provider Aruba. Although bringing a little bit of infrastructural diversity to the overall Nym
+			network, it is admittedly not an ideal solution. Therefore the goal is to migrate the node to
+			another local Czech provider.`),this.h()},l(t){R(n.$$.fragment,t),g=w(t),r=f(t,"SECTION",{class:!0});var u=m(r);e=f(u,"DIV",{class:!0});var i=m(e);s=f(i,"P",{class:!0});var C=m(s);_=k(C,`I am a nym enthusiast who belives one's privacy is too high a price to pay for using the
+			internet. Nym network could bring an alternative option. However I belive that for the network
+			to be trustworthy it needs to be diversified - that is, diversified in many aspects.`),C.forEach(a),x=w(i),l=f(i,"P",{class:!0});var N=m(l);I=k(N,`Take for instance infrastructure. As of now, large portion of mixnet nodes is running on the
+			infrastructure of only a small number of cloud providers. What is the purpose of mixnet
+			splitting traffic among numerous nodes if these nodes are running on the infrastructure of a
+			single provider?`),N.forEach(a),T=w(i),c=f(i,"P",{class:!0});var z=m(c);A=k(z,`Unfortunately a monitoring tool is missing that would evaluate the nodes not only from the
+			economical point of view (the more you stake, the more you earn) but would also take into
+			account aspects such as geolocation, infrustructure, etc. In short, it is difficult to decide
+			which nodes to stake to if you want to contribute to the health of a diversified network
+			rather than simply seek the highest profit. Being a rust/blockchain developer, I have some
+			ideas regarding such a tool... STAY TUNED!`),z.forEach(a),E=w(i),h=f(i,"P",{class:!0});var S=m(h);$=k(S,`My first real contribution to the general idea hinted above is my node. It is running at a
+			local datacenter in the Czech Republic, which is owned by a rather large Italian cloud
+			provider Aruba. Although bringing a little bit of infrastructural diversity to the overall Nym
+			network, it is admittedly not an ideal solution. Therefore the goal is to migrate the node to
+			another local Czech provider.`),S.forEach(a),i.forEach(a),u.forEach(a),this.h()},h(){p(s,"class","mb-8"),p(l,"class","mb-8"),p(c,"class","mb-8"),p(h,"class","mb-8"),p(e,"class","flex flex-col items-start"),p(r,"class","text-gray-200 overflow-hidden")},m(t,u){W(n,t,u),P(t,g,u),P(t,r,u),o(r,e),o(e,s),o(s,_),o(e,x),o(e,l),o(l,I),o(e,T),o(e,c),o(c,A),o(e,E),o(e,h),o(h,$),v=!0},p:Y,i(t){v||(q(n.$$.fragment,t),v=!0)},o(t){F(n.$$.fragment,t),v=!1},d(t){O(n,t),t&&a(g),t&&a(r)}}}class K extends D{constructor(n){super(),B(this,n,null,j,H,{})}}export{K as default};
